a4573548b523ce3a077a092ffce0b4d1627d31a8
Savan <sawanpatel2508@gmail.com>
2023-08-12 23:05:22 -0400

    React component

---
+++ my-react-app/src/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const List = () => {
+    return (
+        <ul>
+            <li>Bacon</li>
+            <li>Ice Cream</li>
+            <li>Noodles</li>
+        </ul>
+    )
+}
+export default List
